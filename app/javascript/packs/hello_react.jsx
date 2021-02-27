013b8a06c59d68e71a47688fa2afc4390b0109c3
Sayam Kim <sayamkim@gmail.com>
2021-02-27 16:40:14 -0600

    Add additional React tests to welcome page and add Actiontext to project.

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -4,23 +4,23 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 const Hello = props => (
   <div>Hello {props.name}!</div>
 )
 
-Hello.defaultProps = {
-  name: 'David'
-}
+// Hello.defaultProps = {
+//   name: 'David'
+// }
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
+// Hello.propTypes = {
+//   name: PropTypes.string
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Hello name="Sayam" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
---
+++ app/javascript/packs/welcome.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+import Counter from 'components/counter'
+
+const Welcome = (props) => {
+  return (
+    <div>
+      <h1>Welcome from React</h1>
+      <Counter count={5} />
+    </div>
+  )
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Welcome />,
+    document.getElementById('welcome'),
+  )
+})
